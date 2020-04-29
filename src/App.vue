74eaa565c147610b8d5bed91f8a06599a82e7b04
nelson <nelsonjcastillos96@gmail.com>
2020-04-29 12:13:55 -0400

    sass y pug

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    h1 {{msg}}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola mundo desde vue'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+</style>
