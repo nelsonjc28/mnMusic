3f42e7f3dc5371ed3b9617081934a13a515c0c0a
nelson <nelsonjcastillos96@gmail.com>
2020-05-05 09:27:26 -0400

    expresiones

---
+++ src/Expresiones.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+ h1 {{msg}}
+  p {{ 1 + 1}}
+  p {{ 'hola' +'Mundo'}}
+  p {{ person.name}}
+  p {{ person.name.toUpperCase()}}
+  p {{ JSON.stringify(person)}}
+  p {{ true ? 'true':'false'}}
+</template>
+
+<script>
+export default {
+  name: 'expresiones',
+  data () {
+    return {
+      msg: 'Hola mundo desde vue',
+      person: {name:'juan'}
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Home from './Home.vue'
+import Expresiones from './Expresiones.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
@@ -8,13 +9,11 @@ import { faFire } from '@fortawesome/free-solid-svg-icons'
 import {faHeart} from '@fortawesome/free-solid-svg-icons'
 import {faCog} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {faPopcorn} from '@fortawesome/vue-fontawesome'
 
 library.add(faHome)
 library.add(faFire)
 library.add(faHeart)
 library.add(faCog)
-// library.add(faPopcorn)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
@@ -22,5 +21,5 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   el: '#app',
-  render: h => h(Home)
+  render: h => h(Expresiones)
 })
