3eea70532968ff00de563f95e53d46bf379974b2
nelson <nelsonjcastillos96@gmail.com>
2020-05-05 08:20:51 -0400

    segundo problema

--- src/Home.vue
+++ src/Home.vue
@@ -3,6 +3,7 @@
     <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="#">
+          <!--          <font-awesome-icon icon="popcorn" size="lg"></font-awesome-icon>-->
           Movie.es
         </a>
 
@@ -14,7 +15,7 @@
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-menu">
         <div class="navbar-start">
         </div>
         <div class="navbar-center is-8" style="width: 700px">
@@ -30,20 +31,209 @@
         </div>
       </div>
     </nav>
-    <aside class="menu is-primary">
-      <p class="menu-label">
-        General
-      </p>
-      <ul class="menu-list">
-        <li>
-          <span class="icon">
-            <icon :icon="['fas','fa-home']" />
-          </span>
-        </li>
+    <div class="columns">
 
-      </ul>
+      <aside class="menu column is-1">
+        <ul class="menu-list">
+          <li>
+            <a class="is-vcentered">
+              <font-awesome-icon icon="home" size="lg"></font-awesome-icon>
+              <div>Home</div>
+            </a>
+          </li>
+          <li>
+            <a>
+              <font-awesome-icon icon="fire" size="lg"></font-awesome-icon>
+              <div>Trending</div>
+            </a>
+          </li>
+          <li>
+            <a>
+              <font-awesome-icon icon="heart" size="lg"></font-awesome-icon>
+              <div>Mas likes</div>
+            </a>
+          </li>
+          <li>
+            <a>
+            <span class="is-vcentered">
+            <font-awesome-icon icon="cog" size="lg"></font-awesome-icon>
+            Ajustes
+              </span>
+            </a>
+          </li>
+        </ul>
 
-    </aside>
+      </aside>
+      <div class="column is-11">
+        <div class="columns">
+          <div class="column is-12">
+            <div class="content">
+              <br>
+              <h2 class="subtitle">Ahora en cine</h2>
+
+            </div>
+
+          </div>
+        </div>
+
+        <div class="columns">
+
+          <div class="box column is-4">
+            <img src="./assets/pelicula.png" class="image">
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        <div class="box column is-4">
+          <img src="./assets/pelicula.png" class="image">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="box  column is-4">
+          <img src="./assets/pelicula.png" class="image">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+        <div class="columns">
+          <div class="column is-12">
+            <div class="content">
+              <h2 class="subtitle">Aclamadas por la critica</h2>
+
+            </div>
+
+          </div>
+        </div>
+
+
+        <div class="columns is-3">
+
+        <div class="box  column is-4">
+          <img src="./assets/pelicula.png" class="image">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box  column is-4">
+          <img src="./assets/pelicula.png" class="image">
+
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box column is-4">
+          <img src="./assets/pelicula.png" class="image">
+
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        <div class="columns">
+          <div class="column is-12">
+            <div class="content">
+              <h2 class="subtitle">No te debes perder</h2>
+
+            </div>
+
+          </div>
+        </div>
+      <div class="columns">
+        <div class="box column is-4">
+          <img src="./assets/pelicula.png" class="image">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box column is-4">
+          <img src="./assets/pelicula.png" class="image">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box column is-4">
+          <img src="./assets/pelicula.png" class="image">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
 
   </div>
 </template>
@@ -62,5 +252,12 @@
 <style lang="scss">
   @import "./scss/main.scss";
 
+  .styleImg {
+    /*border-radius: 50px 50px  0px 0px;*/
+  }
+
+  .styleCard {
+    /*margin: 2px; border-radius: 50px;*/
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ import App from './App.vue'
 import Home from './Home.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import {faHeart} from '@fortawesome/free-solid-svg-icons'
+import {faCog} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {faPopcorn} from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
+library.add(faHome)
+library.add(faFire)
+library.add(faHeart)
+library.add(faCog)
+// library.add(faPopcorn)
 
-Vue.component('icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 
 new Vue({
